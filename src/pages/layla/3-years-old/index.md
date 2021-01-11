917d4e7bfe4f834e3a5987566038ae3517288568
Vanessa Ramos <deepbane@gmail.com>
2021-01-10 20:46:44 -0800

    added layla folder

---
+++ src/pages/layla/3-years-old/index.md
@@ -0,0 +1,7 @@
+---
+title: 3 Years Old
+subtitle: This is an optional subtitle for the portfolio page.
+layout_style: mosaic
+template: layla-years
+year: 3-year-old
+---
---
+++ src/templates/layla.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import _ from 'lodash';
+import {graphql} from 'gatsby';
+
+import {Layout} from '../components/index';
+import {getPages, Link, withPrefix} from '../utils';
+
+// this minimal GraphQL query ensures that when 'gatsby develop' is running,
+// any changes to content files are reflected in browser
+export const query = graphql`
+  query($url: String) {
+    sitePage(path: {eq: $url}) {
+      id
+    }
+  }
+`;
+
+export default class Portfolio extends React.Component {
+    render() {
+        let display_projects = _.orderBy(getPages(this.props.pageContext.pages, '/layla'), 'frontmatter.date', 'desc');
+        return (
+            <Layout {...this.props}>
+            <div className="inner outer">
+              <header className="page-header inner-sm">
+                <h1 className="page-title line-top">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
+                {_.get(this.props, 'pageContext.frontmatter.subtitle', null) && (
+                <p className="page-subtitle">{_.get(this.props, 'pageContext.frontmatter.subtitle', null)}</p>
+                )}
+              </header>
+
+              <ul>
+              <li><a href="/layla/2-years-old">2 year old</a></li>
+              <li><a href="/layla/2-years-old">3 year old</a></li>
+              <li><a href="/layla/2-years-old">4 year old</a></li>
+              </ul>
+            </div>
+            </Layout>
+        );
+    }
+}
